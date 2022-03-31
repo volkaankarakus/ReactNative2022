05767670ac12c7d1004e882f143510e79e6ddc07
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2022-04-01 00:38:56 +0300

    Update Register.js

--- reactnative2022/4-reactNavigation/7-loginAppFormikAndYup/src/Screens/Register/Register.js
+++ reactnative2022/4-reactNavigation/7-loginAppFormikAndYup/src/Screens/Register/Register.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { Component, useEffect, useState } from 'react';
 
-export default function Register() {
+
+
+export default function Register({route,navigation}) {
+  
+  console.log(route);
+  console.log(navigation);
+
+  //const route = useRoute();
+  //const {username,password,email} = useState('');
+
   return (
-    <View>
-      <Text>Register</Text>
+    <View style= {{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>Name : {username}</Text>
     </View>
   )
 }
